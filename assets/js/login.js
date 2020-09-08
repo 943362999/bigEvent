f6a95f2cb97b0417501b7c85a54b2fd6625f3b79
943362999 <943362999@qq.com>
2020-09-08 17:17:05 +0800

    login

---
+++ assets/js/login.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('#login-link').on('click', function () {
+        $('#login-box').hide()
+        $('#reg-box').show()
+
+    })
+    $('#login-reg').on('click', function () {
+        $('#reg-box').hide();
+        $('#login-box').show();
+
+    })
+
+
+})
